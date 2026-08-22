b3bec1a70ff5334c5c9c1011b1b7fe341488ece5
whocaresleft <francesco.biribo.m01@gmail.com>
2026-08-22 17:24:29 +0200

    Refactored Id to unify the cases for empty string and whitespaces only.
    Since "".trim() is still "", and both branches return the same exception
    message, they were merged.

--- hospital-scheduler-domain/src/main/java/org/duckdns/whocaresleft/core/Id.java
+++ hospital-scheduler-domain/src/main/java/org/duckdns/whocaresleft/core/Id.java
@@ -7,8 +7,9 @@ public class Id {
     public static Id createId(String value) {
         
         if (value == null) throw new IllegalArgumentException("Id value cannot be null");
-        if (value == "") throw new IllegalArgumentException("Id value cannot be empty");
-        if (value.trim().isEmpty())  throw new IllegalArgumentException("Id value cannot be empty");
+        
+        String trimmed = value.trim();
+        if (trimmed.isEmpty())  throw new IllegalArgumentException("Id value cannot be empty");
         
         return null;
     }
