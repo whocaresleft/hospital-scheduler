85916394081570c77bb00b452ce44e0d79a0c4b6
whocaresleft <francesco.biribo.m01@gmail.com>
2026-08-23 23:12:05 +0200

    Re-started development of DoctorPresenter.
    Created DoctorPresenterTest, on first test the entity Doctor is missing,
    creating it first netx

--- hospital-scheduler-presentation/src/main/java/org/duckdns/whocaresleft/presenter/DoctorPresenter.java
+++
@@ -1,5 +0,0 @@
-package org.duckdns.whocaresleft.presenter;
-
-public class DoctorPresenter {
-
-}
--- hospital-scheduler-presentation/src/test/java/org/duckdns/whocaresleft/presenter/DoctorPresenterTest.java
+++ hospital-scheduler-presentation/src/test/java/org/duckdns/whocaresleft/presenter/DoctorPresenterTest.java
@@ -1,12 +1,23 @@
 package org.duckdns.whocaresleft.presenter;
 
+import org.assertj.core.util.Arrays;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.mockito.InOrder;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.List;
 
 class DoctorPresenterTest {
 
     @Nested
     class ExceptionalCases {
         
+        @Test
+        void testAllDoctorsShouldLoadAllDoctorsFromRepositoryAndDisplayThemOnView() {
+            List<Doctor> doctors = Arrays.asList();
+        }
     }
 }
