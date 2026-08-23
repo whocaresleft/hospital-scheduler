abfd2a2805e28bfabc50c6d5e0b628efaac30795
whocaresleft <francesco.biribo.m01@gmail.com>
2026-08-23 23:03:01 +0200

    Started developing DoctorPresenter

---
+++ hospital-scheduler-presentation/src/main/java/org/duckdns/whocaresleft/presenter/DoctorPresenter.java
@@ -0,0 +1,5 @@
+package org.duckdns.whocaresleft.presenter;
+
+public class DoctorPresenter {
+
+}
---
+++ hospital-scheduler-presentation/src/test/java/org/duckdns/whocaresleft/presenter/DoctorPresenterTest.java
@@ -0,0 +1,12 @@
+package org.duckdns.whocaresleft.presenter;
+
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+class DoctorPresenterTest {
+
+    @Nested
+    class ExceptionalCases {
+        
+    }
+}
