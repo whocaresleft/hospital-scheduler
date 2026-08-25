2c33f3fe01e40456d73dcd3117ee770cc2ae4908
whocaresleft <francesco.biribo.m01@gmail.com>
2026-08-25 08:20:38 +0200

    Started development of MariaDBDoctorRepository using IT, testcontainers

---
+++ hospital-scheduler-domain/src/it/java/org/duckdns/whocaresleft/repository/mariadb/MariaDBDoctorRepositoryTestcontainersIT.java
@@ -0,0 +1,16 @@
+package org.duckdns.whocaresleft.repository.mariadb;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.containers.MariaDBContainer;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@Testcontainers @DisplayName("Integration tests for MariaDoctorRepository using testcontainers")
+class MariaDBDoctorRepositoryTestcontainersIT {
+
+    @Container
+    private static final MariaDBContainer maria = new MariaDBContainer("mariadb:10.3.39");
+    
+    private MariaDBClient client;
+}
